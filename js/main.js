267f57ae5095c8ddfa532b2eead5b0968bd7d380
zhang <mitmiky@outlook.com>
2017-12-05 19:29:18 +0800

    添加技能条特效和更改loading

--- js/main.js
+++ js/main.js
@@ -10,16 +10,46 @@ all.onclick= function(){
   vallina.onclick= function(){
     portfolioBar.className = 'Bar state-3'
   }
-  
-  window.onscroll = function (xxxxx){
-    (window.scrollY > 0)?topNavBar.classList.add('sticky'):topNavBar.classList.remove('sticky')
-   }
 
-   //加载动画
-var stressing=document.getElementById('stressing')
+ //添加offset类
+  let specialTags = document.querySelectorAll('[data-x]')
+  for(let i =0;i<specialTags.length; i++){
+    specialTags[i].classList.add('offset')
+  }
+  //改变导航条的padding
+  find()
+  window.onscroll = function (xx){
+    (window.scrollY > 0)?topNavBar.classList.add('sticky'):topNavBar.classList.remove('sticky')
+    find()
+   }
+   
+
+    // minIndex离窗口顶部最近的元素
+  function find(){
+    let specialTags = document.querySelectorAll('[data-x]')
+    let minIndex = 0
+    for(let i =1;i<specialTags.length; i++){
+    if(Math.abs(specialTags[i].offsetTop - window.scrollY) < Math.abs(specialTags[minIndex].offsetTop - window.scrollY)){
+      minIndex = i
+    }
+  }
+  
+  specialTags[minIndex].classList.remove('offset')
+  let id = specialTags[minIndex].id
+  let a = document.querySelector('a[href="#'+ id + '"]')
+  let li = a.parentNode
+  let brothersAndMe = li.parentNode.children
+  for(let i=0; i<brothersAndMe.length; i++){
+    brothersAndMe[i].classList.remove('bright')
+  }
+  li.classList.add('bright')
+}
+
+   //loading动画
+  var stressing=document.getElementById('load')
    setTimeout(function(){  
    stressing.classList.remove('active')  
-  }  , 200);
+  }  , 2000);
 
   //点击菜单定位
   let aTags = document.querySelectorAll('nav > ul > li > a')
@@ -34,9 +64,9 @@ var stressing=document.getElementById('stressing')
   console.log(aTags)
   for(let i=0; i<aTags.length; i++){
   aTags[i].onclick = function(x){
-  x.preventDefault()                       //阻止默认动作
+  x.preventDefault()                  //阻止默认动作
   let a = x.currentTarget
-  let href = a.getAttribute('href') //'#siteAbout'
+  let href = a.getAttribute('href')   
   let element = document.querySelector(href)
   let top = element.offsetTop
   // window.scrollTo(0, top - 80)
@@ -70,11 +100,4 @@ for(let i=0; i<liTags.length; i++){
   }
 }
 
-// document.onselectstart=function(){
-//   alert ("你想干哈")
-//   return false}
-// window.oncontextmenu = function () {
-//     alert("你点了右键")
-//     return false;
-   
-//  }
+
